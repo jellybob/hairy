7f2c79ad72b0b20331012fd5c1f1f3bf7137c109
Jon Wood <jon@blankpad.net>
2012-10-01 22:55:41 +0100

    Throw an error if no option selected

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -8,6 +8,10 @@ class VotesController < ApplicationController
   end
 
   def create
+    unless params.key?(:answer)
+      redirect_to(root_path, alert: "You should probably pick an option first") and return
+    end
+
     Vote.create(answer: params[:answer], ip_address: request.remote_ip, user_agent: request.env["HTTP_USER_AGENT"])
 
     cookies[:responded] = true
