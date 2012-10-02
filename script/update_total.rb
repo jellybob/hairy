fec9a192ad216a4a2b64297898a24c6d604d449c
Jon Wood <jon@blankpad.net>
2012-10-02 17:55:10 +0100

    Remove stuff from the regexp

--- script/update_total.rb
+++ script/update_total.rb
@@ -6,7 +6,7 @@ require 'mechanize'
 
 agent = Mechanize.new
 page = agent.get "http://uk.virginmoneygiving.com/fundraiser-web/fundraiser/showFundraiserProfilePage.action?userUrl=jonwood"
-total = page.search("dl dd:first").text.strip.gsub(/\302\243/, "")
+total = page.search("dl dd:first").text.strip
 
 # Write it
 File.open(File.join(File.dirname(__FILE__), "..", "tmp", "total.txt"), "w") { |f| f.print(total) }
