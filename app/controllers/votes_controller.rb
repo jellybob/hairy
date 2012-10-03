1b09d87a933eeba3304cf7dda3f6f3e729a29466
Jon Wood <jon@blankpad.net>
2012-10-03 17:10:24 +0100

    Add an updater to pull the latest total from the giving site

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,10 @@ class VotesController < ApplicationController
   end
 
   def edit
+    unless params[:key] == "icanupdatethis"
+      redirect_to root_path and return
+    end
+
     @total = Totalizer.total
     @updater_active = true
     render :thanks and return
@@ -13,5 +17,10 @@ class VotesController < ApplicationController
   def update
     Totalizer.additional = params[:amount].to_f
     redirect_to "/vote/edit"
+  end
+
+  def update_total
+    Totalizer.update_base_amount
+    render text: "Done"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 HairSurvey::Application.routes.draw do
   resource :vote
-
+  match "/update_total" => "votes#update_total"
   root :to => "votes#index"
 end
